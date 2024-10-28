90e5ed87c9c68d9ae461eb64ec583147b1820875
Ruarke Bullock <Ruarke_Bullock@outlook.com>
2024-10-28 10:45:14 -0400

    Add project files.

---
+++ StoryLine/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace StoryLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.topLabel.Location = new System.Drawing.Point(51, -2);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(310, 90);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "Start";
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(51, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // option1Label
+            // 
+            this.option1Label.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.option1Label.Location = new System.Drawing.Point(119, 291);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(229, 47);
+            this.option1Label.TabIndex = 3;
+            this.option1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option2Label
+            // 
+            this.option2Label.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.option2Label.Location = new System.Drawing.Point(119, 344);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(229, 47);
+            this.option2Label.TabIndex = 4;
+            this.option2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(51, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 47);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // option3Label
+            // 
+            this.option3Label.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.option3Label.Location = new System.Drawing.Point(119, 397);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(229, 47);
+            this.option3Label.TabIndex = 6;
+            this.option3Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Image = global::StoryLine.Properties.Resources.Screen_Shot_2018_11_24_at_5_51_21_PM_768x495;
+            this.label1.Location = new System.Drawing.Point(51, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 207);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(51, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(405, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.topLabel);
+            this.Name = "Form1";
+            this.Text = "DIDDIES MANSION";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ StoryLine/Form1.cs
@@ -0,0 +1,475 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoryLine
+{
+    public partial class Form1 : Form
+    {
+        // tracks what page of the story the user is at
+        int page = 1;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ///Check what page we are currently on, and then flip
+            ///to the page you need to go to if you selected option 1
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+
+            else if (page == 2)
+            {
+                page = 5;
+            }
+
+            else if (page == 3)
+            {
+                page = 99;
+            }
+
+            else if (page == 4)
+            {
+                page = 99;
+            }
+
+            else if (page == 5)
+            {
+                page = 7;
+            }
+
+            else if (page == 6)
+            {
+                page = 99;
+            }
+
+            else if (page == 7)
+            {
+                page = 10;
+            }
+
+            else if (page == 8)
+            {
+                page = 99;
+            }
+
+            else if (page == 9)
+            {
+                page = 99;
+            }
+
+            else if (page == 10)
+            {
+                page = 12;
+            }
+
+            else if (page == 11)
+            {
+                page = 99;
+            }
+
+            else if (page == 12)
+            {
+                page = 14;
+            }
+
+            else if (page == 13)
+            {
+                page = 99;
+            }
+
+            else if (page == 14)
+            {
+                page = 16;
+            }
+
+            else if (page == 15)
+            {
+                page = 99;
+            }
+
+            else if (page == 16)
+            {
+                page = 18;
+            }
+
+            else if (page == 17)
+            {
+                page = 99;
+            }
+
+            else if (page == 18)
+            {
+                page = 20;
+            }
+
+            else if (page == 19)
+            {
+                page = 99;
+            }
+
+            else if (page == 20)
+            {
+                page = 99;
+            }
+            else if (page == 99)
+            {
+                page = 1;
+            }
+
+
+            DisplayPage();
+
+
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 3;
+            }
+            else if (page == 2)
+            {
+                page = 4;
+            }
+            else if (page == 3)
+            {
+                page = 99;
+            }
+
+            else if (page == 4)
+            {
+                page = 99;
+            }
+
+            else if (page == 5)
+            {
+                page = 6;
+            }
+
+            else if (page == 6)
+            {
+                page = 99;
+            }
+
+            else if (page == 7)
+            {
+                page = 9;
+            }
+
+            else if (page == 8)
+            {
+                page = 99;
+            }
+
+            else if (page == 9)
+            {
+                page = 99;
+            }
+
+            else if (page == 10)
+            {
+                page = 11;
+            }
+
+            else if (page == 11)
+            {
+                page = 99;
+            }
+
+            else if (page == 12)
+            {
+                page = 13;
+            }
+
+            else if (page == 13)
+            {
+                page = 99;
+            }
+
+            else if (page == 14)
+            {
+                page = 15;
+            }
+
+            else if (page == 15)
+            {
+                page = 99;
+            }
+
+            else if (page == 16)
+            {
+                page = 17;
+            }
+
+            else if (page == 17)
+            {
+                page = 99;
+            }
+
+            else if (page == 18)
+            {
+                page = 19;
+            }
+
+            else if (page == 19)
+            {
+                page = 99;
+            }
+
+            else if (page == 20)
+            {
+                page = 99;
+            }
+
+            else if (page == 99)
+            {
+                page = 1;
+            }
+
+            DisplayPage();
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+
+            else if (page == 2)
+            {
+                page = 5;
+            }
+
+            else if (page == 3)
+            {
+                page = 99;
+            }
+
+            else if (page == 4)
+            {
+                page = 99;
+            }
+
+            else if (page == 5)
+            {
+                page = 7;
+            }
+
+            else if (page == 6)
+            {
+                page = 99;
+            }
+
+            else if (page == 7)
+            {
+                page = 8;
+            }
+
+            else if (page == 8)
+            {
+                page = 99;
+            }
+
+            else if (page == 9)
+            {
+                page = 99;
+            }
+
+            else if (page == 10)
+            {
+                page = 12;
+            }
+
+            else if (page == 11)
+            {
+                page = 99;
+            }
+
+            else if (page == 12)
+            {
+                page = 14;
+            }
+
+            else if (page == 13)
+            {
+                page = 99;
+            }
+
+            else if (page == 14)
+            {
+                page = 16;
+            }
+
+            else if (page == 15)
+            {
+                page = 99;
+            }
+
+            else if (page == 16)
+            {
+                page = 18;
+            }
+
+            else if (page == 17)
+            {
+                page = 99;
+            }
+
+            else if (page == 18)
+            {
+                page = 20;
+            }
+
+            else if (page == 19)
+            {
+                page = 99;
+            }
+
+            else if (page == 20)
+            {
+                page = 99;
+            }
+            else if (page == 99)
+            {
+                page = 1;
+            }
+
+
+            DisplayPage();
+        }
+
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    topLabel.Text = "you are in a big mansion";
+                    option1Label.Text = "go right";
+                    option2Label.Text = "go left";
+                    break;
+                case 2:
+                    topLabel.Text = "you have come to a stop";
+                    option1Label.Text = "left";
+                    option2Label.Text = "right";
+                    break;
+                case 3:
+                    topLabel.Text = "you come across P.Diddy. He molests you. Play Again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "No";
+                    break;
+                case 4:
+                    topLabel.Text = "you fall down and P.Diddy molests you. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 5:
+                    topLabel.Text = "you walk by food do you eat it?";
+                    option1Label.Text = "no";
+                    option2Label.Text = "yes";
+                    break;
+                case 6:
+                    topLabel.Text = "the food was amazing. but it was laced and you die. Play again.";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 7:
+                    topLabel.Text = "you walk by the food and come to across 3 doors";
+                    option1Label.Text = "red";
+                    option2Label.Text = "green";
+                    option3Label.Text = "blue";
+                    break;
+                case 8:
+                    topLabel.Text = "you call into a pit and die. Play Again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 9:
+                    topLabel.Text = "you slip and fall on his baby oil. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 10:
+                    topLabel.Text = "you come across diddy's room, but he heard you. where do you hide";
+                    option1Label.Text = "in the closet";
+                    option2Label.Text = "under the bed";
+                    break;
+                case 11:
+                    topLabel.Text = "Diddy finds you with no clothes on. Play Again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 12:
+                    topLabel.Text = "you find a hidden door in the cloest. do you go threw?";
+                    option1Label.Text = "leave";
+                    option2Label.Text = "go threw";
+                    break;
+                case 13:
+                    topLabel.Text = "hes at the other side and is waiting for you. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 14:
+                    topLabel.Text = "you make it out of the house how do you leave";
+                    option1Label.Text = "car";
+                    option2Label.Text = "bike";
+                    break;
+                case 15:
+                    topLabel.Text = "he catches you becasue you were too slow. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 16:
+                    topLabel.Text = "you get away but run out of gas. what way do you go?";
+                    option1Label.Text = "south";
+                    option2Label.Text = "north";
+                    break;
+                case 17:
+                    topLabel.Text = "you run back toawds the house and he finds you. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 18:
+                    topLabel.Text = "you find yourself cornered by diddy. denfend yourself";
+                    option1Label.Text = "shovel";
+                    option2Label.Text = "big branch";
+                    break;
+                case 19:
+                    topLabel.Text = "he takes the branch from you and eats you. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 20:
+                    topLabel.Text = "you are able to get away because you killed him. Play again?";
+                    option1Label.Text = "yes";
+                    option2Label.Text = "no";
+                    break;
+                case 99:
+                    break;
+                default:
+                    break;
+            }
+        }
+    
+    }
+}
